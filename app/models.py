dbb5efa22867c2ee6c398bc3b3fedcb0ee948c33
nicolassss16 <nicomerlini@live.com.ar>
2025-06-22 20:56:30 -0300

    models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
+import uuid
 from . import db
 
-class Event(db.Model): # database configuration
+class Event(db.Model):  # Event se queda igual
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
@@ -9,6 +10,7 @@ class Ticket(db.Model):
     name = db.Column(db.String(100), nullable=False)
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    qr_code = db.Column(db.Text, nullable=False)
+    qr_code = db.Column(db.Text, nullable=False)  # Imagen base64
+    ticket_code = db.Column(db.String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
     usado = db.Column(db.Boolean, default=False)
 
