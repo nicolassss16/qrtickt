e2c5d502c2514df7cf04be386e35c16e5bda38ef
nicolassss16 <nicomerlini@live.com.ar>
2025-06-22 14:43:54 -0300

    run.py

--- run.py
+++ run.py
@@ -4,3 +4,9 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+
+# Asegurate de tener esto al final del archivo
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render define PORT como variable de entorno
+    app.run(host='0.0.0.0', port=port)
