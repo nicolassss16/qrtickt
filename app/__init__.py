c873995159174d7a88820e6c666d5ec9ec56ff68
nicolassss16 <nicomerlini@live.com.ar>
2025-06-23 11:09:15 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
-# Initialize extensions requirements
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_object('config.Config')
 
-    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
 
-    # Import and register blueprints
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
