60976e814301ec5fdcabbf8906dd0f472398d523
nicolassss16 <nicomerlini@live.com.ar>
2025-06-24 22:18:29 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -19,5 +19,7 @@ class Ticket(db.Model):
     qr_code = db.Column(db.Text, nullable=False)
     ticket_code = db.Column(db.String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
     usado = db.Column(db.Boolean, default=False)
+    payment_method = db.Column(db.String(50))        # <-- Agregado
+    transaction_id = db.Column(db.String(36))        # <-- Agregado
 
     event = db.relationship('Event', backref=db.backref('tickets', lazy=True))
