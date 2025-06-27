892b5e085b9f1495e8c3e1e143e20a457c26e4ab
nicolassss16 <nicomerlini@live.com.ar>
2025-06-26 21:08:22 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,8 +15,14 @@ def create_app():
     migrate.init_app(app, db)
     login_manager.init_app(app)
     login_manager.login_view = 'main.login'
+    login_manager.login_message = "Por favor, inicia sesión para acceder a esta página."
+    login_manager.login_message_category = "warning"
 
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Opcional para dev sin migraciones:
+    with app.app_context():
+        db.create_all()
 
     return app
