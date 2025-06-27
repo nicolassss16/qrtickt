1d385e6aa4113d0f77fa9c5a14f6e8c35ccadf5e
nicolassss16 <nicomerlini@live.com.ar>
2025-06-26 21:11:48 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os
+
 class Config:
-    SECRET_KEY = 'secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///tickets.db'
+    SECRET_KEY = os.environ.get('secret_key') or 'secret_key_de_desarrollo'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///tickets.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
